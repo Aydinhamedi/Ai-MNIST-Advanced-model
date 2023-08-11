fedc15c8cd9f1117ec3d17c25c46cc274a848259
Aydin <108932477+Aydinhamedi@users.noreply.github.com>
2023-08-11 23:22:02 +0330

    modified:   .gitignore 	modified:   Test program/Setup.py 	modified:   pre-trained model/MNIST_model.h5 	modified:   requirements.txt 	new file:   sample input/image2.jpg 	new file:   sample input/image3.png 	new file:   sample input/image4.png

--- Test program/Setup.py
+++ Test program/Setup.py
@@ -1,5 +1,5 @@
 from keras.models import load_model
-from PIL import Image
+from PIL import Image, ImageFilter
 import numpy as np
 import os
 import matplotlib.pyplot as plt
@@ -31,6 +31,7 @@ except FileNotFoundError:
         sys.exit()
     
 img = img.resize((28, 28))
+img = img.filter(ImageFilter.GaussianBlur(0.5))
 img_array = np.array(img)
 img_array = img_array.reshape(1, 28, 28, 1)
 
@@ -38,88 +39,9 @@ img_array = img_array.reshape(1, 28, 28, 1)
 predictions = model.predict(img_array)
 predicted_digit = np.argmax(predictions)
 confidence = predictions[0][predicted_digit]
-print(f'The predicted digit is {predicted_digit} with a confidence of {confidence:.2f}')
-
-# Create directories if they don't exist
-if not os.path.exists('IMG_ITER'):
-    os.mkdir('IMG_ITER')
-if not os.path.exists('IMG_ITER/white'):
-    os.mkdir('IMG_ITER/white')
-if not os.path.exists('IMG_ITER/black'):
-    os.mkdir('IMG_ITER/black')
-
-# Apply threshold and save images
-thresholds = range(20, 250, 5)
-white_confidences = []
-black_confidences = []
-inverted_white_confidences = []
-inverted_black_confidences = []
-digit_counts = np.zeros(10)
-digit_confidences = np.zeros(10)
-for threshold in thresholds:
-    W_threshold = threshold if threshold <= 195 else 195
-    white_thresholded_img = img.point(lambda x: 255 if x < W_threshold else x)
-    white_thresholded_img.save(f'IMG_ITER/white/threshold_{W_threshold}.png')
-    white_thresholded_img_array = np.array(white_thresholded_img)
-    white_thresholded_img_array = white_thresholded_img_array.reshape(1, 28, 28, 1)
-    predictions = model.predict(white_thresholded_img_array)
-    predicted_digit = np.argmax(predictions)
-    confidence = predictions[0][predicted_digit]
-    white_confidences.append(confidence)
-    digit_counts[predicted_digit] += 1
-    digit_confidences[predicted_digit] += confidence
-    print(f'Threshold: {W_threshold}, Color: white, Predicted digit: {predicted_digit}, Confidence: {confidence:.2f}')
+print(f'The predicted digit is {predicted_digit} with a confidence of {confidence:.3f}')
+print('other digits:')
+for i in range(len(predictions[0])):
+    print(f'{i} with a confidence of {predictions[0][i]:.3f}')
     
-    black_thresholded_img = img.point(lambda x: 0 if x < threshold else x)
-    black_thresholded_img.save(f'IMG_ITER/black/threshold_{threshold}.png')
-    black_thresholded_img_array = np.array(black_thresholded_img)
-    black_thresholded_img_array = black_thresholded_img_array.reshape(1, 28, 28, 1)
-    predictions = model.predict(black_thresholded_img_array)
-    predicted_digit = np.argmax(predictions)
-    confidence = predictions[0][predicted_digit]
-    black_confidences.append(confidence)
-    digit_counts[predicted_digit] += 1
-    digit_confidences[predicted_digit] += confidence
-    print(f'Threshold: {threshold}, Color: black, Predicted digit: {predicted_digit}, Confidence: {confidence:.2f}')
 
-    inverted_white_thresholded_img = img.point(lambda x: x if x < W_threshold else 255 - x)
-    inverted_white_thresholded_img_array = np.array(inverted_white_thresholded_img)
-    inverted_white_thresholded_img_array = inverted_white_thresholded_img_array.reshape(1, 28, 28, 1)
-    predictions = model.predict(inverted_white_thresholded_img_array)
-    predicted_digit = np.argmax(predictions)
-    confidence = predictions[0][predicted_digit]
-    inverted_white_confidences.append(confidence)
-    digit_counts[predicted_digit] += 1
-    digit_confidences[predicted_digit] += confidence
-
-    inverted_black_thresholded_img = img.point(lambda x: x if x < threshold else 255 - x)
-    inverted_black_thresholded_img_array = np.array(inverted_black_thresholded_img)
-    inverted_black_thresholded_img_array = inverted_black_thresholded_img_array.reshape(1, 28, 28, 1)
-    predictions = model.predict(inverted_black_thresholded_img_array)
-    predicted_digit = np.argmax(predictions)
-    confidence = predictions[0][predicted_digit]
-    inverted_black_confidences.append(confidence)
-    digit_counts[predicted_digit] += 1
-    digit_confidences[predicted_digit] += confidence
-# Create graph
-plt.plot(thresholds, white_confidences, label='White')
-plt.plot(thresholds, black_confidences, label='Black')
-plt.plot(thresholds, inverted_white_confidences, label='Inverted White')
-plt.plot(thresholds, inverted_black_confidences, label='Inverted Black')
-plt.xlabel('Threshold')
-plt.ylabel('Confidence')
-plt.legend()
-plt.show()
-
-# Display most predicted digits and their average confidence
-digit_counts = digit_counts / np.sum(digit_counts)
-digit_scores = np.zeros(10)
-for digit in range(10):
-    if digit_counts[digit] > 0:
-        avg_confidence = digit_confidences[digit] / ((digit_counts[digit] * len(thresholds) * 2) * 2)
-        print(f'Digit: {digit}, Percentage: {digit_counts[digit]:.2f}, Average Confidence: {avg_confidence:.2f}')
-        digit_scores[digit] = digit_counts[digit] + (avg_confidence / 3)
-
-best_digit = np.argmax(digit_scores)
-best_score = digit_scores[best_digit]
-print(f'\nThe best result is for digit {best_digit} with a score of {best_score:.2f}')
